e4236e4946b900f0bdb13da0db522515343d0820
teruya-sawamura <t.s.16125@gmail.com>
2020-06-20 23:59:00 +0900

    api post clear

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,25 @@
 class SearchController < ApplicationController
   def index
-    require 'unirest'
-    response = Unirest.get "https://edamam-edamam-nutrition-analysis.p.rapidapi.com/api/nutrition-data?ingr=1+large+apple",
-      headers:{
-        "X-RapidAPI-Host" => "edamam-edamam-nutrition-analysis.p.rapidapi.com",
-        "X-RapidAPI-Key" => "RAPIDAPI_KEY"
-      }
+    require 'uri'
+    require 'net/http'
+    require 'openssl'
 
-    @result = JSON.parse(response.raw_body)
+    url = URI("https://edamam-edamam-nutrition-analysis.p.rapidapi.com/api/nutrition-data?ingr=1%20large%20apple")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["x-rapidapi-host"] = 'edamam-edamam-nutrition-analysis.p.rapidapi.com'
+    request["x-rapidapi-key"] = ENV["RAPIDAPI_KEY"]
+
+    response = http.request(request)
+
+    result = JSON.parse(response.body)
+
+    @label = result["totalNutrients"]["PROCNT"]["label"]
+    @quantity = result["totalNutrients"]["PROCNT"]["quantity"]
+    @unit = result["totalNutrients"]["PROCNT"]["unit"]
   end
 end
