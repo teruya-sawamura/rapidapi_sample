0a604cafb8ae59d0b7d6bd8869c874ac8a429396
teruya-sawamura <t.s.16125@gmail.com>
2020-07-05 09:36:00 +0900

    translate and search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,37 +4,39 @@ class SearchController < ApplicationController
     require 'net/http'
     require 'openssl'
 
-    url = URI("https://edamam-edamam-nutrition-analysis.p.rapidapi.com/api/nutrition-data?ingr=1#{params[:search]}")
+    url = URI("https://google-translate1.p.rapidapi.com/language/translate/v2")
 
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-    request = Net::HTTP::Get.new(url)
-    request["x-rapidapi-host"] = 'edamam-edamam-nutrition-analysis.p.rapidapi.com'
+    request = Net::HTTP::Post.new(url)
+    request["x-rapidapi-host"] = 'google-translate1.p.rapidapi.com'
     request["x-rapidapi-key"] = ENV["RAPIDAPI_KEY"]
+    request["accept-encoding"] = 'application/gzip'
+    request["content-type"] = 'application/x-www-form-urlencoded'
+    request.body = "source=ja&q=#{params[:translation]}&target=en"
+
+    response = http.request(request)
+    result = JSON.parse(response.body)
+
+    translated_text = result["data"]["translations"][0]["translatedText"]
 
 
 
-      @response = http.request(request)
 
-      @result = JSON.parse(@response.body)
+    url1 = URI("https://edamam-edamam-nutrition-analysis.p.rapidapi.com/api/nutrition-data?ingr=1#{translated_text}")
+
+    http1 = Net::HTTP.new(url1.host, url1.port)
+    http1.use_ssl = true
+    http1.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request1 = Net::HTTP::Get.new(url1)
+    request1["x-rapidapi-host"] = 'edamam-edamam-nutrition-analysis.p.rapidapi.com'
+    request1["x-rapidapi-key"] = ENV["RAPIDAPI_KEY"]
+
+    response1 = http.request(request1)
+    @result1 = JSON.parse(response1.body)
       
-      
-
-      # total_weight = result["totalWeight"]
-
-      # total_cal = result["calories"]
-      # @cal = total_cal / total_weight * 100
-      
-
-      # @label = result["totalNutrients"]["PROCNT"]["label"]
-
-      # total_quantity = result["totalNutrients"]["PROCNT"]["quantity"]
-      # @quantity = total_quantity / total_weight * 100
-      
-      
-      # @unit = result["totalNutrients"]["PROCNT"]["unit"]
-    
   end
 end
